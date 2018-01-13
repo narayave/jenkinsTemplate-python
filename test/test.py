22ef9125e7303152fc0c0a9948f9d6bb9d46ad8b
Vedanth Narayanan <vedanth.nd@gmail.com>
2018-01-13 13:21:47 -0800

    hoping push works

--- test/test.py
+++ test/test.py
@@ -3,13 +3,16 @@ sys.path.append(os.path.abspath("./src"))
 from arrayPartition1 import Solution
 
 class TestArrayPartition1(unittest.TestCase):
-    def setUp(self):
-        self.sln = Solution()
+	def setUp(self):
+		self.sln = Solution()
 
-    def test_basic(self):
-        self.assertEqual(self.sln.ArrayPairSum([1,4,2,3]), 4)
+	def test_basic(self):
+		self.assertEqual(self.sln.ArrayPairSum([1,4,2,3]), 4)
+
+	def test_trivial(self):
+		self.assertEqual([1,2], [1,2])
 
 
 if __name__ == '__main__':
-    import xmlrunner
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+	import xmlrunner
+	unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
